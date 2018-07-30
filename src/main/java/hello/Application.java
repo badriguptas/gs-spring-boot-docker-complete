bae84a56ceec363c273c0bbcea8f9ed15e85f682
badriguptas <badri.guptas@gmail.com>
2018-07-31 02:14:50 +0530

    Updated Code

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World";
+        return "<B>Hello Docker World<B>";
     }
 
     public static void main(String[] args) {
