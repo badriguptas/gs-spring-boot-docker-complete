4c2ecbf0821c5015d302b951cf7c411d70914e5b
badriguptas <badri.guptas@gmail.com>
2018-08-02 01:34:30 +0530

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.ArrayList;
 
 @SpringBootApplication
 @RestController
 public class Application {
-
+    
+    private List<Feedback> feedbacks = new ArrayList<>();
+   
     @RequestMapping("/")
     public String home() {
         return "<B>Hello Docker World<B>";
@@ -17,5 +21,14 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
+    
+    @RequestMapping("/feedbacks")
+    public List<Feedback> feedbacks() {
+        return this.feedbacks;
+    }
+    
+    @RequestMapping("/feeback")
+    public String addFeedback() {
+        return "";
+    }
 }
