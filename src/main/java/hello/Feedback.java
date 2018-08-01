a52e77f4a95690d72db3b6a01df91b1984817732
admin <badri.guptas@gmail.com>
2018-08-02 02:09:09 +0530

    Added Feedback

---
+++ src/main/java/hello/Feedback.java
@@ -0,0 +1,29 @@
+package hello;
+
+
+public class Feedback {
+    
+
+    private String comments;
+    private String name;
+    private String experience;
+    
+	public String getComments() {
+		return comments;
+	}
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getExperience() {
+		return experience;
+	}
+	public void setExperience(String experience) {
+		this.experience = experience;
+	}
+}
