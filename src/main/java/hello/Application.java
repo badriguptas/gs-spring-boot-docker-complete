c61cba329f9f5bd340cbb2f654069c0e67ddbf6e
badriguptas <badri.guptas@gmail.com>
2018-08-02 14:31:23 +0530

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMethod.POST;
+import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
 import java.util.ArrayList;
 
